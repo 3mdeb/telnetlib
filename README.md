b85e3b5e6068eb3441cb21badb5a266301839e61
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2024-12-02 10:55:36 +0100

    initial commit
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Telnetlib
+
+`telnetlib/__init__.py` was copied from
+<https://github.com/python/cpython/blob/3.12/Lib/telnetlib.py> with one line
+removed:
+
+```python
+warnings._deprecated(__name__, remove=(3, 13))
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='telnetlib',
+    version='1.0.0',
+    url='https://github.com/3mdeb/telnetlib',
+    description='Copy of Python 3.12 telnetlib library',
+    packages=["telnetlib"],
+)
